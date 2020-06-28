6c43845225babec6e98d903d23d3c7e71a762bf7
Mr Footwork <pandau.ting@gmail.com>
2020-06-28 21:51:00 +0200

    Image gallery example Finished
    This project has teached me how to use DOM manipulation in JavaScript. It is quite difficult to get used to the syntax. Also there are many different ways to use event handlers.

--- JS Building Blocks - Image Gallery/main.js
+++ JS Building Blocks - Image Gallery/main.js
@@ -4,10 +4,22 @@ const thumbBar = document.querySelector('.thumb-bar')
 const btn = document.querySelector('button')
 const overlay = document.querySelector('.overlay')
 
-/* Looping through images */
-
-const newImage = document.createElement('img')
-newImage.setAttribute('src', xxx)
-thumbBar.appendChild(newImage)
+for (let i = 1; i <= 5; i++) {
+	const newImage = document.createElement('img')
+	newImage.setAttribute('src', `images/pic${i}.jpg`)
+	thumbBar.appendChild(newImage)
+	newImage.onclick = (e) => (displayedImage.src = e.target.src)
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = (e) => {
+	if (btn.getAttribute('class') == 'light') {
+		btn.setAttribute('class', 'dark')
+		btn.textContent = 'Lighten'
+		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+	} else {
+		btn.setAttribute('class', 'light')
+		btn.textContent = 'Darken'
+		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+	}
+}
