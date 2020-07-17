b979c1901a99b017dc77f371d18d671d96c53f92
Mr Footwork <pandau.ting@gmail.com>
2020-07-17 17:43:22 +0200

    5 hüpfende Bälle

--- JS 4 - OOJS/7 Object Building/main.js
+++ JS 4 - OOJS/7 Object Building/main.js
@@ -8,10 +8,56 @@ const height = (canvas.height = window.innerHeight)
 
 //TODO: https://developer.mozilla.org/de/docs/Learn/JavaScript/Objects/Object_building_practice
 
-//balls
-let balls = []
+class Ball {
+	constructor(x, y, velX, velY, color, size) {
+		this.x = x
+		this.y = y
+		this.velX = velX
+		this.velY = velY
+		this.color = color
+		this.size = size
+	}
+	draw() {
+		ctx.beginPath()
+		ctx.fillStyle = this.color
+		ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+		ctx.fill()
+	}
+	update() {
+		if (this.x + this.size >= width) {
+			this.velX = -this.velX
+		}
+		if (this.x - this.size <= 0) {
+			this.velX = -this.velX
+		}
+		if (this.y + this.size >= height) {
+			this.velY = -this.velY
+		}
+		if (this.y - this.size <= 0) {
+			this.velY = -this.velY
+		}
+		this.x += this.velX
+		this.y += this.velY
+	}
+	collisionDetect() {
+		for (let j = 0; j < balls.length; j++) {
+			if (!(this === balls[j])) {
+				const dx = this.x - balls[j].x
+				const dy = this.y - balls[j].y
+				const distance = Math.sqrt(dx ** 2 + dy ** 2)
 
-while (balls.length < 50) {
+				if (distance < this.size + balls[j].size) {
+					balls[j].color = this.color =
+						'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')'
+				}
+			}
+		}
+	}
+}
+
+//instantiating balls
+let balls = []
+while (balls.length < 5) {
 	let size = random(10, 20)
 	let ball = new Ball(
 		// ball position always drawn at least one ball width
@@ -23,54 +69,15 @@ while (balls.length < 50) {
 		'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
 		size
 	)
-
 	balls.push(ball)
 }
 
 // function to generate random number
-
 function random(min, max) {
-	const num = Math.floor(Math.random() * (max - min + 1)) + min
-	return num
+	return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function Ball(x, y, velX, velY, color, size) {
-	this.x = x
-	this.y = y
-	this.velX = velX
-	this.velY = velY
-	this.color = color
-	this.size = size
-}
-
-Ball.prototype.draw = function() {
-	ctx.beginPath()
-	ctx.fillStyle = this.color
-	ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
-	ctx.fill()
-}
-
-Ball.prototype.update = function() {
-	if (this.x + this.size >= width) {
-		this.velX = -this.velX
-	}
-
-	if (this.x - this.size <= 0) {
-		this.velX = -this.velX
-	}
-
-	if (this.y + this.size >= height) {
-		this.velY = -this.velY
-	}
-
-	if (this.y - this.size <= 0) {
-		this.velY = -this.velY
-	}
-
-	this.x += this.velX
-	this.y += this.velY
-}
-
+//Zeichnung und Animation der Bälle
 function loop() {
 	ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'
 	ctx.fillRect(0, 0, width, height)
@@ -78,6 +85,7 @@ function loop() {
 	for (let i = 0; i < balls.length; i++) {
 		balls[i].draw()
 		balls[i].update()
+		balls[i].collisionDetect()
 	}
 
 	requestAnimationFrame(loop)
